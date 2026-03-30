a68d4c2ddffb0c52ac0ed01948fdcecebca03f51
Alexandre Agneray <122207242+alexagneray@users.noreply.github.com>
2026-03-30 18:10:21 +0200

    On recommence - Suppression de tous les fichiers

--- server/UserInfo.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-struct UserInfo
-{
-    std::string _name;
-    std::string _passwd;
-};
--- server/action/DataPublicationActor.cpp
+++
@@ -1,9 +0,0 @@
-#include <DataPublicationSession.h>
-
-class DataPublicationActor
-{
-protected:
-    DataPublicationSession m_Session;
-    
-
-};
